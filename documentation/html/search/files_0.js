10b4e0cb197b2ce98a4916e64e87564d6a047cba
Karsten Reitan Sørensen <karsten.reitan@gmail.com>
2022-12-20 11:20:32 +0100

    Update documentation

---
+++ documentation/html/search/files_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['dbconfig_2ephp_0',['dbconfig.php',['../dbconfig_8php.html',1,'']]]
+];
---
+++ documentation/html/search/variables_0.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['db_5fdatabase_0',['DB_DATABASE',['../dbconfig_8php.html#a4798f8ff41893eb12308c2b638b8becf',1,'dbconfig.php']]],
+  ['db_5fpassword_1',['DB_PASSWORD',['../dbconfig_8php.html#a4dca144fafcc7bd3f71cb5778afcdd13',1,'dbconfig.php']]],
+  ['db_5fserver_2',['DB_SERVER',['../dbconfig_8php.html#a046861af9c950733d95478526a7e19ae',1,'dbconfig.php']]],
+  ['db_5fuser_3',['DB_USER',['../dbconfig_8php.html#a1d1d99f8e08f387d84fe9848f3357156',1,'dbconfig.php']]]
+];
