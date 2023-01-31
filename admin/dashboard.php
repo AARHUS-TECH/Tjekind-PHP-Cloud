4490f7359e615af753f2bff8432844b582e893a3
Kaki-the-Kid <karsten.reitan@gmail.dk>
2023-01-31 10:58:40 +0100

    Update bootstrap5.2.3 -popper.js2.11.6

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -48,12 +48,12 @@ else if(isset($_REQUEST['status']) == 1)
 ?><!DOCTYPE html>
 <html>
 	<head>
-	<meta charset="UTF-8">
-		<title>Tjek ind | Aarhus Tech SKP</title>
+		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Tjek ind | Aarhus Tech SKP</title>
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
 		<link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.min.css">
-		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 		<!-- link rel="stylesheet" type="text/css" href="/assets/css/bootstrap.min.css" -->
 		<link rel="stylesheet" type="text/css" href="/assets/css/custom.css">
 		<link rel="stylesheet" type="text/css" href="/assets/css/mobile.css">
@@ -365,9 +365,8 @@ else if(isset($_REQUEST['status']) == 1)
 		</div>
 				
 		<script src="https://code.jquery.com/jquery-3.6.3.min.js"></script>
-		<script src="https://unpkg.com/@popperjs/core@2/dist/umd/popper.js"></script>
-		<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
-		<script src="/assets/js/dismiss-alerts.js"></script>
+		<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>		<script src="/assets/js/dismiss-alerts.js"></script>
 		<script src="/assets/js/sortlist.js"></script>
 		
 		<script>
