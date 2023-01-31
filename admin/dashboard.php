1761493fade35f31927d87209db313b08d091a8c
Kaki-the-Kid <karsten.reitan@gmail.dk>
2023-01-31 10:08:42 +0100

    Not compatible

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -367,12 +367,10 @@ else if(isset($_REQUEST['status']) == 1)
 				
 		<script src="/assets/js/jquery.min.js"></script>
 		<script src="/assets/js/popper.js"></script>
-		<!-- script src="/assets/js/bootstrap.min.js"></!-->
+		<script src="/assets/js/bootstrap.min.js"></script>
 		<script src="/assets/js/dismiss-alerts.js"></script>
 		<script src="/assets/js/sortlist.js"></script>
-		<!-- script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></!-->
-		<!-- script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></!-->
-		<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js" integrity="sha384-+sLIOodYLS7CIrQpBjl+C7nPvqq+FbNUBDunl/OZv93DB7Ln/533i8e/mZXLi/P+" crossorigin="anonymous"></script>
+		<script src="/assets/js/scroll.js"></script>
 		
 		<script>
 			var y = <?php echo (isset($_REQUEST['y']))?$_REQUEST['y']:0; ?>;
