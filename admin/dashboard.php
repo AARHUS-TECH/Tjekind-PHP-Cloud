48f1a554098d7b7809ee3b7706461cfd216e7621
Kaki-the-Kid <karsten.reitan@gmail.dk>
2023-01-31 10:30:09 +0100

    Testing new popper.js

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -364,11 +364,10 @@ else if(isset($_REQUEST['status']) == 1)
 			</div>
 		</div>
 				
-		<!-- script src="/assets/js/jquery.min.js"></!-->
 		<script src="https://code.jquery.com/jquery-3.6.3.min.js"></script>
-		<script src="/assets/js/popper.js"></script>
-		<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
-		<!--script src="/assets/js/bootstrap.min.js"></!--script></!-->
+		<script src="https://unpkg.com/@popperjs/core@2/dist/umd/popper.js"></script>
+		<!-- script src="/assets/js/popper.js"></!-->
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
 		<script src="/assets/js/dismiss-alerts.js"></script>
 		<script src="/assets/js/sortlist.js"></script>
 		
