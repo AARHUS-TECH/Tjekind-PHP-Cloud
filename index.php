e80e5b5e14024350dedd98a297dc59a92d00b901
Kaki-the-Kid <karsten.reitan@gmail.dk>
2023-01-31 14:03:49 +0100

    Update bootstrap

--- index.php
+++ index.php
@@ -8,11 +8,16 @@ $user->auth();
 <!DOCTYPE html>
 <html>
     <head>
-        <meta charset="utf-8">
-        <title>Tjek ind | Aarhus Tech SKP</title>
+    <meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Tjek ind | Aarhus Tech SKP</title>
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
+		<link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.min.css">
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+		<!-- link rel="stylesheet" type="text/css" href="/assets/css/bootstrap.min.css" -->
+		<link rel="stylesheet" type="text/css" href="/assets/css/custom.scss">
+		<link rel="stylesheet" type="text/css" href="/assets/css/mobile.scss">
 
-        <link rel="stylesheet" type="text/css" href="/assets/css/bootstrap.min.css">
-        <link rel="stylesheet" type="text/css" href="/assets/css/custom.css">
         <script>
             function startTime() {
                 var today = new Date();
