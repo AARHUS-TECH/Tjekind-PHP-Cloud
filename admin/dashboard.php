d1373ff6ecf9f3357e7f546f2c6ef8d1947b3b02
Kaki-the-Kid <karsten.reitan@gmail.dk>
2023-01-31 10:17:00 +0100

    Testign New bootstrap

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -52,7 +52,8 @@ else if(isset($_REQUEST['status']) == 1)
 		<title>Tjek ind | Aarhus Tech SKP</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
-		<link rel="stylesheet" type="text/css" href="/assets/css/bootstrap.min.css">
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+		<!-- link rel="stylesheet" type="text/css" href="/assets/css/bootstrap.min.css" -->
 		<link rel="stylesheet" type="text/css" href="/assets/css/custom.css">
 		<link rel="stylesheet" type="text/css" href="/assets/css/mobile.css">
 		<link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.min.css">
