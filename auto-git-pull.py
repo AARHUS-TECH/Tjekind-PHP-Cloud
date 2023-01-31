ff6eb301ef595fcb07072687ac1f29af13e64b8a
Kaki-the-Kid <karsten.reitan@gmail.dk>
2023-01-31 08:56:09 +0100

    Debugging

--- auto-git-pull.py
+++ auto-git-pull.py
@@ -22,24 +22,25 @@ path = "/var/www/cloud" # "/home/username/website"
 log_path = "/var/www/cloud/logs/auto-git-pull.log" # "/home/username/website/logs/auto-git-pull.log"
 
 # Define the time between each pull
-time_between_pulls = 5 # 5 minutes (5 * 60 seconds)
+time_between_pulls = 20 # 5 minutes (5 * 60 seconds)
 
 # Define the time between each log
-time_between_logs = 3600 # 1 hour
+time_between_logs = time_between_pulls # 3600 = 1 hour
 
 ## code that set up time variables and make git pull based on time
 last_pull = 0
 last_log = 0    
 while True:
     if time.time() - last_pull > time_between_pulls:
-        lst_pull = time.time()
+        last_pull = time.time()
         print("cd " + path + " && git pull") if (DEBUG) else None
-        #os.system("cd " + path + " && git pull")
+        os.system("cd " + path + " && git pull")
+    # Log the time of the pull
     if time.time() - last_log > time_between_logs:
         last_log = time.time()
+        print("Auto git pull at " + str(datetime.datetime.now()) + "") if (DEBUG) else None
         #with open(log_path, "a") as f:
         #    f.write("Auto git pull at " + str(datetime.datetime.now()) + "")
-        print
             
             
 
