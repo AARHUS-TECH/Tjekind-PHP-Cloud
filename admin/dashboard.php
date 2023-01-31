6bd97ce36870172a79a1b7aba3badeff6b9622c6
Kaki-the-Kid <karsten.reitan@gmail.dk>
2023-01-31 12:44:41 +0100

    Update colorscheme

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -55,8 +55,8 @@ else if(isset($_REQUEST['status']) == 1)
 		<link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.min.css">
 		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 		<!-- link rel="stylesheet" type="text/css" href="/assets/css/bootstrap.min.css" -->
-		<link rel="stylesheet" type="text/css" href="/assets/css/custom.css">
-		<link rel="stylesheet" type="text/css" href="/assets/css/mobile.css">
+		<link rel="stylesheet" type="text/css" href="/assets/css/custom.scss">
+		<link rel="stylesheet" type="text/css" href="/assets/css/mobile.scss">
 	</head>
 	
 	<body onload="refreshTable();">
