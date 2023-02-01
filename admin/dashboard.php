5b0e101b16e3904264b69dfe6406cb4c2598cc24
Kaki-the-Kid <karsten.reitan@gmail.dk>
2023-02-01 10:01:21 +0100

    Inaktive section added

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -99,7 +99,7 @@ else if(isset($_REQUEST['status']) == 1)
 						<div class="accordion-item">
 							<h2 class="accordion-header" id="panelsStayOpen-headingOne">
 								<button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
-								Aktive Elever
+									Aktive Elever
 								</button>
 							</h2>
 							<div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show" aria-labelledby="panelsStayOpen-headingOne">
@@ -209,8 +209,19 @@ else if(isset($_REQUEST['status']) == 1)
 							</h2>
 							<div id="panelsStayOpen-collapseTwo" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingTwo">
 								<div class="accordion-body">
-									
-								<strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+									<table class="table table-hover">
+										<thead>
+											<tr>
+												<th>Navn</th>
+												<th>Seneste indtjekning</th>
+												<th>Status</th>
+												<th>Handlinger</th>
+											</tr>
+										</thead>
+				
+										<tbody id='holderTable2'>
+										</tbody>
+									</table> 
 								</div>
 							</div>
 						</div>
