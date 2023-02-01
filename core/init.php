24126d4a385d60c47c636f448a83593b573e3db3
Karsten Reitan Sørensen <karsten.reitan@gmail.com>
2023-02-01 23:00:47 +0100

    Inserted tokens for use in json access

--- core/init.php
+++ core/init.php
@@ -30,9 +30,22 @@ $GLOBALS['config'] = array(
     ),
     'info' => array(
         'tjekind_pc_ip' => '::1'
-    )
+    ),
+    'token-terminal' => array(
+        '5f51a4a917841eb4210b2c97979886d1cdcc1ece77e0bc1a2d49579449baf019',
+        'f2b5407138b561103f17efc76117961ebb5673face973b573341bd50840011eb',
+        '9786e29337c7b5682f5147f375e8552228c5c9f30c76a71c5fb5016967c09204',
+        '26040e8d06d9fa568b3e974308cd822a00f7354b093a28a1780cf68abf9f14c1',
+        '06bb21a25fd6fa618158591e6e6e7ff8369e3c8da859a636d6e94560f510d89d',
+        'bc086ff954bcf61a82783898d129722afe76aa023a66c42b717cdb33d88f45e6',
+        'e76db9531005d20ef4df0ed936ad70405383d26d930f4e66ce04bd0226699f6c',
+        'd8ce464525b69c35f72e37407976ddbc4e42debadbc3c56eedd6bf4492ad0fe5',
+        '8218e6b61fdb2f6e48de118502b014c7bb6aff83a9cfacbdf436e9b7f53209f5',
+        '17a59e2dbdac1ee974ae1424f68baf67615a95ebf743ffc739e3a05d38d06b64',
+    ),
+    'token-webunits'=>array(),
 );
 
 spl_autoload_register(function($class) {
-    require_once $_SERVER["DOCUMENT_ROOT"] . '/cloud/classes/' . $class . '.php';
+    require_once $_SERVER["DOCUMENT_ROOT"] . '/classes/' . $class . '.php';
 });
