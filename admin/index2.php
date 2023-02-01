15322bfb184b76f80ab17f552aa4f75abb75b95b
Kaki-the-Kid <karsten.reitan@gmail.dk>
2023-02-01 12:35:36 +0100

    Update to new js scripts

--- admin/index2.php
+++ admin/index2.php
@@ -134,10 +134,12 @@ $userdata = $user->getInfo(Session::get('userID'));
             </div>
         </div>
 
-        <script src="/assets/js/jquery.min.js"></script>
-        <script src="/assets/js/popper.js"></script>
-        <script src="/assets/js/bootstrap.min.js"></script>
+        <script src="https://code.jquery.com/jquery-3.6.3.min.js"></script>
+		<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>		<script src="/assets/js/dismiss-alerts.js"></script>
+		<script src="/assets/js/sortlist.js"></script>
         <script src="/assets/js/dismiss-alerts.js"></script>
-        <script src="/assets/js/sortlist.js"></script>
+        <script src="/assets/js/login-page.js"></script>
+
     </body>
 </html>
