22c38872181503325f891c27362afa92c987e74b
Kaki-the-Kid <karsten.reitan@gmail.dk>
2023-02-01 08:34:13 +0100

    Header to Updated Bootstrap css etc

--- admin/redigerElev.php
+++ admin/redigerElev.php
@@ -71,20 +71,15 @@ if($updatedata['iSKP'] == 1) {
 <!DOCTYPE html>
 <html>
 	<head>
-		<meta charset="utf-8">
+	<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<title>Tjek ind | Aarhus Tech SKP</title>
-
-		<link rel="stylesheet" type="text/css" href="/assets/css/bootstrap.min.css">
-		<link rel="stylesheet" type="text/css" href="/assets/css/custom.css">
-	</head>
-		<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css">
-		<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js">
-
-		<link rel="stylesheet" type="text/css" href="/assets/css/bootstrap.min.scss">
-		<link rel="stylesheet" type="text/css" href="/assets/css/custom.scss">
-
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
 		<link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.min.css">
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+		<!-- link rel="stylesheet" type="text/css" href="/assets/css/bootstrap.min.css" -->
+		<link rel="stylesheet" type="text/css" href="/assets/css/custom.scss">
+		<link rel="stylesheet" type="text/css" href="/assets/css/mobile.scss">
 	</head>s
 	<body>
 		<div class="container">
