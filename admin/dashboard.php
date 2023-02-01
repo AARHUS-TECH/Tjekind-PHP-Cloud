623e93fdf056a1779e577762d21162f6c6b1d28f
Kaki-the-Kid <karsten.reitan@gmail.dk>
2023-02-01 10:07:25 +0100

    Cleaning up

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -226,102 +226,80 @@ else if(isset($_REQUEST['status']) == 1)
 							</div>
 						</div>
 
-						<div class="accordion-item">
-						<h2 class="accordion-header" id="panelsStayOpen-headingThree">
-							<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseThree" aria-expanded="false" aria-controls="panelsStayOpen-collapseThree">
-							Accordion Item #3
-							</button>
-						</h2>
-						</div>
-						</div>
+					</div>
 
 
+					<!-- region Modalvindue #for at tjekke kort data -->
+					<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+						<div class="modal-dialog modal-dialog-centered" role="document">
+							<div class="modal-content">
+								<div class="modal-header">
+									<h5 class="modal-title" id="exampleModalLongTitle">Skan kortet</h5>
+									<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+									<span aria-hidden="true">&times;</span>
+									</button>
+								</div>
 
-
-
-
-					<div class="card card-default">
-
-
-
-
-
-
-
-
-
-						<div id="splash" class="card-body">
-							<!-- region Modalvindue #for at tjekke kort data -->
-							<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-								<div class="modal-dialog modal-dialog-centered" role="document">
-									<div class="modal-content">
-										<div class="modal-header">
-											<h5 class="modal-title" id="exampleModalLongTitle">Skan kortet</h5>
-											<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-											<span aria-hidden="true">&times;</span>
-											</button>
-										</div>
-
-										<div class="modal-body">
-											<div class="form-group">
-												<input 
-													type="password" 
-													class="form-control" 
-													id="inputCardData" 
-													aria-describedby="emailHelp" 
-													placeholder="Kort data"
-													value=""						
-												>
-												<div 
-													id="cardDataHelp" 
-													class="form-text"
-												>Ingen kortdata</div>
-											</div>
-										</div>
-
-										<div class="modal-footer">
-											<button 
-												type="button" 
-												class="btn btn-secondary" 
-												data-dismiss="modal"
-												onclick="document.getElementById('inputCardData').value='';document.getElementById('cardDataHelp').innerHTML='Ingen kortdata';"
-												>Luk</button>
-											<button 
-												type="submit" 
-												class="btn btn-primary"
-												onclick="(document.getElementById('inputCardData').value != '')?showCardData():'';"
-											>Tjek kort</button>
-										</div>
+								<div class="modal-body">
+									<div class="form-group">
+										<input 
+											type="password" 
+											class="form-control" 
+											id="inputCardData" 
+											aria-describedby="emailHelp" 
+											placeholder="Kort data"
+											value=""						
+										>
+										<div 
+											id="cardDataHelp" 
+											class="form-text"
+										>Ingen kortdata</div>
 									</div>
 								</div>
+
+								<div class="modal-footer">
+									<button 
+										type="button" 
+										class="btn btn-secondary" 
+										data-dismiss="modal"
+										onclick="document.getElementById('inputCardData').value='';document.getElementById('cardDataHelp').innerHTML='Ingen kortdata';"
+										>Luk</button>
+									<button 
+										type="submit" 
+										class="btn btn-primary"
+										onclick="(document.getElementById('inputCardData').value != '')?showCardData():'';"
+									>Tjek kort</button>
+								</div>
 							</div>
+						</div>
+					</div>
 
-							<script>
-							function showCardData() {
-								var str = document.getElementById('inputCardData').value;
-								console.log(str);
+					<script>
+					function showCardData() {
+						var str = document.getElementById('inputCardData').value;
+						console.log(str);
 
-								if (str.length == 0) {
-								 	document.getElementById("cardDataHelp").innerHTML = "Input feltet er tomt - indgiv kortkode";
-								 	return;
-								} else {
-									document.getElementById("cardDataHelp").innerHTML = "Tjekker kortet op mod databasen";
+						if (str.length == 0) {
+							document.getElementById("cardDataHelp").innerHTML = "Input feltet er tomt - indgiv kortkode";
+							return;
+						} else {
+							document.getElementById("cardDataHelp").innerHTML = "Tjekker kortet op mod databasen";
 
-								 	const xmlhttp = new XMLHttpRequest();
-								 	xmlhttp.onload = function() {
-								 		msg = this.responseText;
-								 		console.log(msg);
-								 		document.getElementById("cardDataHelp").innerHTML = this.responseText;
-								 	}
-
-								 	xmlhttp.open("GET", "/admin/ajaxResponse.php?card=" + str);
-								  	xmlhttp.send();
-								}
-
-								document.getElementById('inputCardData').value='';
+							const xmlhttp = new XMLHttpRequest();
+							xmlhttp.onload = function() {
+								msg = this.responseText;
+								console.log(msg);
+								document.getElementById("cardDataHelp").innerHTML = this.responseText;
 							}
-							</script>
-							<!-- endregion: Modalvindue -->
+
+							xmlhttp.open("GET", "/admin/ajaxResponse.php?card=" + str);
+							xmlhttp.send();
+						}
+
+						document.getElementById('inputCardData').value='';
+					}
+					</script>
+					<!-- endregion: Modalvindue -->
 
 							<br />
 							<center>
@@ -329,8 +307,8 @@ else if(isset($_REQUEST['status']) == 1)
 							</center>
 						</div>
 					</div>
-				</div>
-			</div>
+
+
 		</div>
 				
 		<script src="https://code.jquery.com/jquery-3.6.3.min.js"></script>
