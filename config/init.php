d22932544c8c141ae65acd1f9bfb4bbb49fc4001
Kaki-the-Kid <karsten.reitan@gmail.dk>
2023-02-02 18:08:51 +0100

    Testing remote access

--- config/init.php
+++ config/init.php
@@ -9,10 +9,10 @@ session_start();
 
 $GLOBALS['config'] = array(
     'database' => array(
-        'dsn'       => 'mysql:host=localhost;dbname=ats_skpdatait_dk_tjekind',       # Database Host
-        'username'  => 'ats_skpdatait_dk_tjekind',                                           # Database Brugernavn
-        'password'  => 'Datait2022!@one.com',                                  # Database Password
-        'charset'   => 'utf8',                                           # Database Charset
+        'dsn'       => 'mysql:host=localhost;dbname=ats_skpdatait_dk_tjekind',  # Database Host
+        'username'  => 'ats_skpdatait_dk_tjekind',                              # Database Brugernavn
+        'password'  => 'Datait2022!@one.com',                                   # Database Password
+        'charset'   => 'utf8',                                                  # Database Charset
 		'dbname'    => 'ats_skpdatait_dk_tjekind',
 		'dbserver'  => 'localhost'
     ),
@@ -42,7 +42,7 @@ $GLOBALS['config'] = array(
         'reserve' => "3Lw~7|Xw8'qq#xQ?AQZ+L85x3Ko|&0",
     ),
     // Token som sendes fra lokale Tjekind enheder ved request
-    'token-terminal' => array(
+    'token_terminal' => array(
         'Servicedesk' => '5f51a4a917841eb4210b2c97979886d1cdcc1ece77e0bc1a2d49579449baf019',
         'Mekaniker'   => 'f2b5407138b561103f17efc76117961ebb5673face973b573341bd50840011eb',
         'Elektriker'  => '9786e29337c7b5682f5147f375e8552228c5c9f30c76a71c5fb5016967c09204',
