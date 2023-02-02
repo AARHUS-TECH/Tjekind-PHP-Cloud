17926cdebda47c22671abf1b374e1d70dee47059
Kaki-the-Kid <karsten.reitan@gmail.dk>
2023-02-02 10:45:58 +0100

    Refactor folder core to config

---
+++ config/dbconfig_default.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @file   dbconfig.h
+ * @brief  Configueation for connection to database
+ * @author Karsten Reitan Sørensen <krs@aarhustech.dk>
+ * @bug    No known bugs
+ */
+
+
+define('DB_USER',     "");   // Your database user name
+define('DB_PASSWORD', "");	     // Your database password (mention your db password here)
+define('DB_DATABASE', "");   // Your database name
+define('DB_SERVER',   "localhost");			         // db server (Mostly will be 'local' host)
+
+?>
